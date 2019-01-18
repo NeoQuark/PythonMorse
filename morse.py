6d9f7e11f66bec559453300c478ae5b31d02ee0f
NeoQuark <quentin.courbin@ynov.com>
2019-01-18 16:57:59 +0100

    [Fixed] word/ char split + [Feature] clean exit

--- morse.py
+++ morse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.5
 # -*- coding: utf-8 -*-
-import os, time
+import sys, os, time
 duration = 0.2  # second
 freq = 440  # Hz
 
@@ -11,27 +11,31 @@ alphabet = {'a': '._', 'b': '_...', 'c': '_._.', 'd': '_..', 'e': '.', 'f': '.._
 			'6': '_....', '7': '__...', '8': '___.', '9': '____.', '0': '_____', ' ': '/'}
 
 while True:
-	msg = str(input("Ecrivez du code morse ou un texte :\n")).replace('-','_').replace('  ', '/')
+	msg = str(input("Ecrivez du code morse ou un texte (/q pour quitter):\n")).replace('-','_').replace('  ', '/')
 	morse = ''
-	try:
-		# A -> .-
-		for c in msg:
-			morse  += alphabet[c.lower()] + ' '
-		print(morse)
-		for c in morse:
-			if c == str('.'):
-				duration = 0.2
-			elif c == str('_'):
-				duration = 0.5
-			else:
-				time.sleep(0.5)
-				continue
-			# os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
-	except KeyError:
-		# .- -> A
-		for word in msg.split('/'):
-			for key, value in alphabet.items():
-				if value == word:
-					morse += key
-			morse += ' '
-		print(morse)
+	if msg != '/q':
+		try:
+			# A -> .-
+			for c in msg:
+				morse  += alphabet[c.lower()] + ' '
+			print(morse)
+			for c in morse:
+				if c == str('.'):
+					duration = 0.2
+				elif c == str('_'):
+					duration = 0.5
+				else:
+					time.sleep(0.5)
+					continue
+				# os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
+		except KeyError:
+			# .- -> A
+			for word in msg.split('/'):
+				for c in word.split(' '):
+					for key, value in alphabet.items():
+						if value == c:
+							morse += key
+				morse += ' '
+			print(morse)
+	else:
+		sys.exit(0)
