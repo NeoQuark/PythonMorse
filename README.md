1d68148036c02662c7cf6a8f8ea8011fa2f0826a
NeoQuark <quentin.courbin@ynov.com>
2019-02-15 15:46:18 +0100

    [Fixed] now handling code spacement according to international morse code

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+![MIT License](https://img.shields.io/github/license/NeoQuark/PythonMorse.svg?style=flat)
+[![Python 3.5](https://img.shields.io/badge/python-3.5-blue.svg?style=flat)](https://www.python.org/downloads/release/python-350/)
+![Push date](https://img.shields.io/date/1550241896.svg?style=flat)
+
 # PythonMorse
 Script that lets you convert alphabet to morse and morse to alphabet.
 
--- morse.py
+++ morse.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3.5
 # -*- coding: utf-8 -*-
-import sys, os, time
-duration = 0.2  # second
+
+from sys import stdout, exit
+from time import sleep
+duration = 0.3  # second
 freq = 440  # Hz
+
+dot = duration
+dash = 3 * dot
 
 alphabet = {'a': '._', 'b': '_...', 'c': '_._.', 'd': '_..', 'e': '.', 'f': '.._.', 'g': '__.', 'h': '....', 
 			'i': '..', 'j': '.___', 'k': '_._', 'l': '._..', 'm': '__', 'n': '_.', 'o': '___', 'p': '.__.', 
@@ -10,29 +15,40 @@ alphabet = {'a': '._', 'b': '_...', 'c': '_._.', 'd': '_..', 'e': '.', 'f': '.._
 			'y': '_.__', 'z': '__..', '1': '.____', '2': '..___', '3': '...__', '4': '...._', '5': '.....', 
 			'6': '_....', '7': '__...', '8': '___.', '9': '____.', '0': '_____', ' ': '/'}
 
+replaceMap = [('-', '_'), ('–', '_'), ('—', '_'), ('•', '.'), ('  ', '/')]
+
 while True:
-	msg = str(input("Ecrivez du code morse ou un texte (/q pour quitter):\n")).replace('-','_').replace('  ', '/')
+	msg = str(input("Ecrivez du code morse ou un texte (/q pour quitter):\n"))
+
+	for c, r in replaceMap:
+		msg = msg.replace(c, r)
 	morse = ''
+
 	if msg != '/q':
 		try:
 			# A -> .-
 			for c in msg:
 				morse  += alphabet[c.lower()] + ' '
-			#print(morse)
 			code = ""
 			for c in morse:
 				code += c
-				sys.stdout.write("\r{}".format(code))
+				stdout.write("\r{}".format(code))
 				if c == str('.'):
-					time.sleep(0.5)
-					duration = 0.5
+					sleep(dot)
+					duration = dot
 				elif c == str('_'):
-					time.sleep(1)
-					duration = 1
+					sleep(dash)
+					duration = dash
+				elif c == str(' '):
+					sleep(dash)
+					duration = dash
+				elif c == str('/'):
+					sleep(dot)
+					duration = dot
 				else:
 					continue
-			print("\n")
-				# os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (duration, freq))
+			print('\n')
+
 		except KeyError:
 			# .- -> A
 			for word in msg.split('/'):
@@ -43,4 +59,4 @@ while True:
 				morse += ' '
 			print(morse)
 	else:
-		sys.exit(0)
+		exit(0)
